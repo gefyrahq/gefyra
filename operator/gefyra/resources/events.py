945b8151e2fc8810a98fbb301fa7b4953294f970
Michael Schilonka <michael@blueshoe.de>
2021-12-18 18:37:05 +0100

    fix(operator): event time format

--- operator/gefyra/resources/events.py
+++ operator/gefyra/resources/events.py
@@ -14,7 +14,7 @@ def create_interceptrequest_established_event(
         reason="Established",
         note=f"This InterceptRequest route on Pod {pod_name} container {container_name}:{container_port}"
         f" has been established",
-        event_time=datetime.now().isoformat(),
+        event_time=datetime.now().isoformat() + "+01:00",
         regarding=k8s.client.V1ObjectReference(
             kind="interceptrequest", name=intercept_request_name, namespace=namespace
         ),
@@ -25,7 +25,7 @@ def create_operator_ready_event(namespace: str) -> k8s.client.EventsV1Event:
     return k8s.client.EventsV1Event(
         reason="Startup",
         note="Operator has been started configured successfully",
-        event_time=datetime.now().isoformat(),
+        event_time=datetime.now().isoformat() + "+01:00",
         regarding=k8s.client.V1ObjectReference(
             kind="deployment", name="gefyra-operator", namespace=namespace
         ),
