4f4019bc2b10803101a324e674f26104cbc31e27
Michael Schilonka <michael@blueshoe.de>
2022-02-22 11:25:52 +0100

    chore(client): remove nsenter probe

--- client/gefyra/local/check.py
+++ client/gefyra/local/check.py
@@ -1,21 +1,8 @@
 import logging
-import subprocess
 
 from gefyra.configuration import ClientConfiguration, default_configuration
 
 logger = logging.getLogger(__name__)
-
-
-def probe_nsenter():
-    logger.debug("Probing: 'nsenter'")
-    try:
-        subprocess.check_call(
-            ["nsenter", "-h"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
-        )
-    except Exception:
-        logger.error("'nsenter' seems to be missing or is not working for Gefyra")
-    else:
-        logger.info("nsenter: Ok.")
 
 
 def probe_docker(config: ClientConfiguration = default_configuration):
@@ -34,6 +21,8 @@ def probe_kubernetes(config: ClientConfiguration = default_configuration):
     try:
         config.K8S_CORE_API.list_namespace()
     except Exception:
-        logger.error("Kubernetes does not seem to be working for Gefyra")
+        logger.error(
+            "Kubernetes is not connected to a cluster or does not seem to be working for Gefyra"
+        )
     else:
         logger.info("Kubernetes: Ok.")
