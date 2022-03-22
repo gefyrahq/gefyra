a576905ba7508b3aba6b26da6c7b805760095151
Michael Schilonka <michael@blueshoe.de>
2022-03-22 21:09:44 +0100

    chore: bump to version 0.6.7

--- client/gefyra/configuration.py
+++ client/gefyra/configuration.py
@@ -10,7 +10,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("gefyra")
 logger.addHandler(console)
 
-__VERSION__ = "0.6.6"
+__VERSION__ = "0.6.7"
 
 
 class ClientConfiguration(object):
