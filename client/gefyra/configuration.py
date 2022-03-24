a791bd7c25d3a6cc0a767eba3fc2578956865675
Robert Stein <robert@blueshoe.de>
2022-03-24 22:58:33 +0100

    chore: bump version to 0.6.15

--- client/gefyra/configuration.py
+++ client/gefyra/configuration.py
@@ -10,7 +10,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("gefyra")
 logger.addHandler(console)
 
-__VERSION__ = "0.6.14"
+__VERSION__ = "0.6.15"
 
 
 class ClientConfiguration(object):
