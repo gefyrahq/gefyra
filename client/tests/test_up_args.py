1761cdf8a86308967b46beae3a5ef60c44c61230
Robert Stein <robert@blueshoe.de>
2022-04-25 13:38:03 +0200

    refactor: reduce code smells (duplicated strings)

--- client/tests/test_up_args.py
+++ client/tests/test_up_args.py
@@ -2,26 +2,34 @@ from gefyra.__main__ import up_parser, up_command
 from gefyra.configuration import ClientConfiguration, __VERSION__
 
 
+REGISTRY_URL = "my-reg.io/gefyra"
+QUAY_REGISTRY_URL = "quay.io/gefyra"
+STOWAWAY_LATEST = "my-reg.io/gefyra/stowaway:latest"
+CARGO_LATEST = "my-reg.io/gefyra/cargo:latest"
+OPERATOR_LATEST = "my-reg.io/gefyra/operator:latest"
+CARRIER_LATEST = "my-reg.io/gefyra/carrier:latest"
+
+
 def test_parse_registry_a():
-    args = up_parser.parse_args(["--registry", "my-reg.io/gefyra"])
+    args = up_parser.parse_args(["--registry", REGISTRY_URL])
     configuration = ClientConfiguration(registry_url=args.registry)
-    assert configuration.REGISTRY_URL == "my-reg.io/gefyra"
+    assert configuration.REGISTRY_URL == REGISTRY_URL
 
 
 def test_parse_registry_b():
     args = up_parser.parse_args(["--registry", "my-reg.io/gefyra/"])
     configuration = ClientConfiguration(registry_url=args.registry)
-    assert configuration.REGISTRY_URL, "my-reg.io/gefyra"
+    assert configuration.REGISTRY_URL, REGISTRY_URL
 
     args = up_parser.parse_args(["-r", "my-reg.io/gefyra/"])
     configuration = ClientConfiguration(registry_url=args.registry)
-    assert configuration.REGISTRY_URL == "my-reg.io/gefyra"
+    assert configuration.REGISTRY_URL == REGISTRY_URL
 
 
 def test_parse_no_registry():
     args = up_parser.parse_args()
     configuration = ClientConfiguration(registry_url=args.registry)
-    assert configuration.REGISTRY_URL == "quay.io/gefyra"
+    assert configuration.REGISTRY_URL == QUAY_REGISTRY_URL
 
 
 def test_parse_no_stowaway_image():
@@ -49,79 +57,71 @@ def test_parse_no_cargo_image():
 
 
 def test_parse_stowaway_image():
-    args = up_parser.parse_args(["--stowaway", "my-reg.io/gefyra/stowaway:latest"])
+    args = up_parser.parse_args(["--stowaway", STOWAWAY_LATEST])
     configuration = ClientConfiguration(stowaway_image_url=args.stowaway)
-    assert configuration.STOWAWAY_IMAGE == "my-reg.io/gefyra/stowaway:latest"
+    assert configuration.STOWAWAY_IMAGE == STOWAWAY_LATEST
 
-    args = up_parser.parse_args(["-s", "my-reg.io/gefyra/stowaway:latest"])
+    args = up_parser.parse_args(["-s", STOWAWAY_LATEST])
     configuration = ClientConfiguration(stowaway_image_url=args.stowaway)
-    assert configuration.STOWAWAY_IMAGE == "my-reg.io/gefyra/stowaway:latest"
+    assert configuration.STOWAWAY_IMAGE == STOWAWAY_LATEST
 
-    args = up_parser.parse_args(
-        ["-s", "my-reg.io/gefyra/stowaway:latest", "-r", "quay.io/gefyra"]
-    )
+    args = up_parser.parse_args(["-s", STOWAWAY_LATEST, "-r", QUAY_REGISTRY_URL])
     configuration = ClientConfiguration(
         registry_url=args.registry, stowaway_image_url=args.stowaway
     )
-    assert configuration.STOWAWAY_IMAGE == "my-reg.io/gefyra/stowaway:latest"
+    assert configuration.STOWAWAY_IMAGE == STOWAWAY_LATEST
 
 
 def test_parse_cargo_image():
-    args = up_parser.parse_args(["--cargo", "my-reg.io/gefyra/cargo:latest"])
+    args = up_parser.parse_args(["--cargo", CARGO_LATEST])
     configuration = ClientConfiguration(cargo_image_url=args.cargo)
-    assert configuration.CARGO_IMAGE == "my-reg.io/gefyra/cargo:latest"
+    assert configuration.CARGO_IMAGE == CARGO_LATEST
 
-    args = up_parser.parse_args(["-a", "my-reg.io/gefyra/cargo:latest"])
+    args = up_parser.parse_args(["-a", CARGO_LATEST])
     configuration = ClientConfiguration(cargo_image_url=args.cargo)
-    assert configuration.CARGO_IMAGE == "my-reg.io/gefyra/cargo:latest"
+    assert configuration.CARGO_IMAGE == CARGO_LATEST
 
-    args = up_parser.parse_args(
-        ["-a", "my-reg.io/gefyra/cargo:latest", "-r", "quay.io/gefyra"]
-    )
+    args = up_parser.parse_args(["-a", CARGO_LATEST, "-r", QUAY_REGISTRY_URL])
     configuration = ClientConfiguration(
         registry_url=args.registry, cargo_image_url=args.cargo
     )
-    assert configuration.CARGO_IMAGE == "my-reg.io/gefyra/cargo:latest"
+    assert configuration.CARGO_IMAGE == CARGO_LATEST
 
 
 def test_parse_operator_image():
-    args = up_parser.parse_args(["--operator", "my-reg.io/gefyra/operator:latest"])
+    args = up_parser.parse_args(["--operator", OPERATOR_LATEST])
     configuration = ClientConfiguration(operator_image_url=args.operator)
-    assert configuration.OPERATOR_IMAGE == "my-reg.io/gefyra/operator:latest"
+    assert configuration.OPERATOR_IMAGE == OPERATOR_LATEST
 
-    args = up_parser.parse_args(["-o", "my-reg.io/gefyra/operator:latest"])
+    args = up_parser.parse_args(["-o", OPERATOR_LATEST])
     configuration = ClientConfiguration(operator_image_url=args.operator)
-    assert configuration.OPERATOR_IMAGE == "my-reg.io/gefyra/operator:latest"
+    assert configuration.OPERATOR_IMAGE == OPERATOR_LATEST
 
-    args = up_parser.parse_args(
-        ["-o", "my-reg.io/gefyra/operator:latest", "-r", "quay.io/gefyra"]
-    )
+    args = up_parser.parse_args(["-o", OPERATOR_LATEST, "-r", QUAY_REGISTRY_URL])
     configuration = ClientConfiguration(
         registry_url=args.registry, operator_image_url=args.operator
     )
-    assert configuration.OPERATOR_IMAGE == "my-reg.io/gefyra/operator:latest"
+    assert configuration.OPERATOR_IMAGE == OPERATOR_LATEST
 
 
 def test_parse_carrier_image():
-    args = up_parser.parse_args(["--carrier", "my-reg.io/gefyra/carrier:latest"])
+    args = up_parser.parse_args(["--carrier", CARRIER_LATEST])
     configuration = ClientConfiguration(carrier_image_url=args.carrier)
-    assert configuration.CARRIER_IMAGE == "my-reg.io/gefyra/carrier:latest"
+    assert configuration.CARRIER_IMAGE == CARRIER_LATEST
 
-    args = up_parser.parse_args(["-c", "my-reg.io/gefyra/carrier:latest"])
+    args = up_parser.parse_args(["-c", CARRIER_LATEST])
     configuration = ClientConfiguration(carrier_image_url=args.carrier)
-    assert configuration.CARRIER_IMAGE == "my-reg.io/gefyra/carrier:latest"
+    assert configuration.CARRIER_IMAGE == CARRIER_LATEST
 
-    args = up_parser.parse_args(
-        ["-c", "my-reg.io/gefyra/carrier:latest", "-r", "quay.io/gefyra"]
-    )
+    args = up_parser.parse_args(["-c", CARRIER_LATEST, "-r", QUAY_REGISTRY_URL])
     configuration = ClientConfiguration(
         registry_url=args.registry, carrier_image_url=args.carrier
     )
-    assert configuration.CARRIER_IMAGE == "my-reg.io/gefyra/carrier:latest"
+    assert configuration.CARRIER_IMAGE == CARRIER_LATEST
 
 
 def test_parse_combination_a():
-    args = up_parser.parse_args(["-c", "my-reg.io/gefyra/carrier:latest"])
+    args = up_parser.parse_args(["-c", CARRIER_LATEST])
     configuration = ClientConfiguration(
         registry_url=args.registry,
         stowaway_image_url=args.stowaway,
@@ -129,13 +129,13 @@ def test_parse_combination_a():
         cargo_image_url=args.cargo,
         carrier_image_url=args.carrier,
     )
-    assert configuration.REGISTRY_URL == "quay.io/gefyra"
+    assert configuration.REGISTRY_URL == QUAY_REGISTRY_URL
     assert configuration.OPERATOR_IMAGE == f"quay.io/gefyra/operator:{__VERSION__}"
-    assert configuration.CARRIER_IMAGE == "my-reg.io/gefyra/carrier:latest"
+    assert configuration.CARRIER_IMAGE == CARRIER_LATEST
 
 
 def test_parse_combination_b():
-    args = up_parser.parse_args(["-r", "my-reg.io/gefyra"])
+    args = up_parser.parse_args(["-r", REGISTRY_URL])
     configuration = ClientConfiguration(
         registry_url=args.registry,
         stowaway_image_url=args.stowaway,
@@ -143,14 +143,14 @@ def test_parse_combination_b():
         cargo_image_url=args.cargo,
         carrier_image_url=args.carrier,
     )
-    assert configuration.REGISTRY_URL == "my-reg.io/gefyra"
+    assert configuration.REGISTRY_URL == REGISTRY_URL
     assert configuration.OPERATOR_IMAGE == f"my-reg.io/gefyra/operator:{__VERSION__}"
     assert configuration.CARRIER_IMAGE == f"my-reg.io/gefyra/carrier:{__VERSION__}"
 
 
 def test_parse_combination_c():
     args = up_parser.parse_args(
-        ["-r", "my-reg.io/gefyra", "-c", "quay.io/gefyra/carrier:latest"]
+        ["-r", REGISTRY_URL, "-c", "quay.io/gefyra/carrier:latest"]
     )
     configuration = ClientConfiguration(
         registry_url=args.registry,
@@ -159,7 +159,7 @@ def test_parse_combination_c():
         cargo_image_url=args.cargo,
         carrier_image_url=args.carrier,
     )
-    assert configuration.REGISTRY_URL == "my-reg.io/gefyra"
+    assert configuration.REGISTRY_URL == REGISTRY_URL
     assert configuration.OPERATOR_IMAGE == f"my-reg.io/gefyra/operator:{__VERSION__}"
     assert configuration.CARRIER_IMAGE == "quay.io/gefyra/carrier:latest"
 
