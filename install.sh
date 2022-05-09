37c98dfb1bf9d5a11908912249c8fa4096686a4a
Georg Krause <mail@georg-krause.net>
2022-05-09 10:32:19 +0200

    Stop pretending not to support darwin arm64

--- install.sh
+++ install.sh
@@ -14,14 +14,6 @@ case $OS in
 		;;
 esac
 
-ARCH="`uname -m`"
-
-if [ "$OS" = "darwin" ] && [ "$ARCH" != "x86_64" ]
-then
-	echo "Currently we only support Darwin amd64/x86_64. Stay tuned!"
-	exit 1
-fi
-
 download_url=$(curl -L -s https://api.github.com/repos/gefyrahq/gefyra/releases/latest | grep '"browser_download_url": ".*'$OS'.*"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*")
 file_name=$(echo $download_url | grep -oE '[^/]+$')
 curl -L $download_url -o /tmp/$file_name
