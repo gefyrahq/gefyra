383cb65a6b633fcd0404c745059cd2f5cf7f942f
Michael Schilonka <michael@blueshoe.de>
2022-07-29 11:12:51 +0200

    chore: bump version to 0.8.4

--- client/gefyra/configuration.py
+++ client/gefyra/configuration.py
@@ -10,7 +10,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("gefyra")
 logger.addHandler(console)
 
-__VERSION__ = "0.8.3"
+__VERSION__ = "0.8.4"
 
 
 def fix_pywin32_in_frozen_build() -> None:
