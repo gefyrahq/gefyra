b3cce18269a3992f74158418a96e7ebc50988ff3
Robert Stein <robert@blueshoe.de>
2022-08-29 16:58:40 +0200

    fix(#175): testing order to comply with `docker run` command

--- client/tests/test_port_parser.py
+++ client/tests/test_port_parser.py
@@ -4,18 +4,19 @@ from gefyra.__main__ import run_parser, bridge_parser
 def test_ip_port_mapper():
     args = run_parser.parse_args(
         [
-            "--expose=8080:localhost:8080",
-            "--expose=9090:127.0.0.1:9090",
-            "--expose=7070:7070",
+            "--expose=localhost:8080:8080",
+            "--expose=127.0.0.1:9090:1234",
+            "--expose=7071:7070",
             "-i=1",
             "-N=test",
         ]
     )
-    assert "9090" in args.expose
+    assert "1234" in args.expose
     assert "8080" in args.expose
-    assert args.expose["9090"] == ("9090", "127.0.0.1")
-    assert args.expose["8080"] == ("8080", "localhost")
-    assert args.expose["7070"] == "7070"
+    assert "7070" in args.expose
+    assert args.expose["1234"] == ("127.0.0.1", "9090")
+    assert args.expose["8080"] == ("localhost", "8080")
+    assert args.expose["7070"] == "7071"
 
 
 def test_port_mapper():
