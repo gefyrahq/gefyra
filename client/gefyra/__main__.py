8631ef7ecb9cfdd0522f9d8a218117c7250990ae
aknorr <adrian.knorr@consol.de>
2022-10-15 00:10:37 +0200

    Refactor bridge target help

--- client/gefyra/__main__.py
+++ client/gefyra/__main__.py
@@ -173,7 +173,9 @@ bridge_parser.add_argument(
 )
 bridge_parser.add_argument(
     "--target",
-    help="Intercept the container given in the notation 'resource/name/containername'. Can be deployment, statefulset or pod. E.g.: --target deployment/hello-nginx/nginx",
+    help="Intercept the container given in the notation 'resource/name/container'. "
+         "Resource can be one of 'deployment', 'statefulset' or 'pod'. "
+         "E.g.: --target deployment/hello-nginx/nginx",
     required=False,
 )
 
