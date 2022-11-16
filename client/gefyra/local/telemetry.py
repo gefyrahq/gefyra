6101b9f449ab856e12b691adae03e94538622975
Robert Stein <SteinRobert@users.noreply.github.com>
2022-11-16 20:06:44 +0100

    Test for unique user id / telemetry (#276)
    * chore: tests and tele adaptions
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- client/gefyra/local/telemetry.py
+++ client/gefyra/local/telemetry.py
@@ -1,5 +1,6 @@
-import configparser
 import logging
+import configparser
+import uuid
 from pathlib import Path
 
 from cli_tracker.sdk import CliTracker
@@ -38,18 +39,28 @@ class CliTelemetry:
             config = self.create_config(gefyra_dir / self.file_name)
         try:
             config["telemetry"].getboolean("track")
+            # This was added later and is here for backwards compatbility
+            user_id = self._get_user_id(config)
         except KeyError:
             config = self.create_config(gefyra_dir / self.file_name)
 
         if config["telemetry"].getboolean("track"):
-            self._init_tracker()
+            self._init_tracker(user_id=user_id)
 
-    def _init_tracker(self):
+    def _init_tracker(self, user_id):
         self.tracker = CliTracker(
             application="gefyra",
             dsn=SENTRY_DSN,
             release=__VERSION__,
+            fingerprint=user_id,
         )
+
+    def _get_user_id(self, config):
+        if config["telemetry"].get("id"):
+            user_id = config["telemetry"].get("id")
+        else:
+            user_id = self.create_id()
+        return user_id
 
     def load_config(self, path):
         config = configparser.ConfigParser()
@@ -59,8 +70,7 @@ class CliTelemetry:
 
     def create_config(self, path):
         config = configparser.ConfigParser()
-        config["telemetry"] = {"track": "True"}
-
+        config["telemetry"] = {"track": "True", "id": str(uuid.uuid4())}
         output_file = Path(path)
         output_file.parent.mkdir(exist_ok=True, parents=True)
 
@@ -68,6 +78,15 @@ class CliTelemetry:
             config.write(config_file)
         self.path = path
         return config
+
+    def create_id(self):
+        config = configparser.ConfigParser()
+        config.read(self.path)
+        user_id = str(uuid.uuid4())
+        config["telemetry"]["id"] = user_id
+        with open(str(self.path), "w") as config_file:
+            config.write(config_file)
+        return user_id
 
     def off(self):
         config = configparser.ConfigParser()
@@ -79,12 +98,14 @@ class CliTelemetry:
             self.tracker.report_opt_out()
         logger.info("Disabled telemetry.")
 
-    def on(self):
+    def on(self, test=False):
         config = configparser.ConfigParser()
         config.read(self.path)
         config["telemetry"]["track"] = "True"
         with open(str(self.path), "w") as config_file:
             config.write(config_file)
-        self._init_tracker()
-        self.tracker.report_opt_in()
+        user_id = self._get_user_id(config)
+        self._init_tracker(user_id=user_id)
+        if not test:
+            self.tracker.report_opt_in()
         logger.info("Enabled telemetry.")
---
+++ client/tests/test_telemetry.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+import unittest
+
+from gefyra.local.telemetry import CliTelemetry
+
+
+class TelemetryTest(unittest.TestCase):
+    config_pre = "/.gefyra/config.ini"
+    config_post = "/.gefyra/config.bak.ini"
+
+    def setUp(self):
+        home = str(Path.home())
+        os.rename(self.config_path, home + self.config_post)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        home = str(Path.home())
+        os.rename(home + self.config_post, self.config_path)
+        return super().tearDown()
+
+    @property
+    def config_path(self):
+        return str(Path.home()) + self.config_pre
+
+    def _init_tracker(self):
+        self.tracker = CliTelemetry()
+        # Make sure events are not sent
+        self.tracker.tracker.sentry._client.transport = None
+
+    def test_config_file_created(self):
+        self.assertFalse(os.path.isfile(self.config_path))
+        self._init_tracker()
+        self.assertTrue(os.path.isfile(self.config_path))
+
+    def test_config_file_read(self):
+        self._init_tracker()
+        config = self.tracker.load_config(self.config_path)
+        self.assertTrue(config["telemetry"].getboolean("track"))
+
+    def test_opt_out(self):
+        self._init_tracker()
+        self.tracker.off()
+        config = self.tracker.load_config(self.config_path)
+        self.assertFalse(config["telemetry"].getboolean("track"))
+
+    def test_opt_in(self):
+        self._init_tracker()
+        self.tracker.on(test=True)
+        config = self.tracker.load_config(self.config_path)
+        self.assertTrue(config["telemetry"].getboolean("track"))
