c77a0c7f1f8726b331d739184cb0131cc0cd2f2d
Robert Stein <robert@blueshoe.de>
2022-11-22 14:09:59 +0100

    chore: add missing file
    Signed-off-by: Robert Stein <robert@blueshoe.de>

---
+++ client/gefyra/local/cargoimage/cargo_dockerfile.py
@@ -0,0 +1,42 @@
+# flake8: noqa
+import io
+import sys
+
+
+def get_dockerfile(cargo_image):
+    run_patch = ""
+    if sys.platform == "win32":
+        run_patch = "RUN patch /usr/bin/wg-quick /wgquick.patch"
+    return io.BytesIO(
+        f""" 
+FROM {cargo_image}
+{run_patch}
+
+ARG ADDRESS
+ARG MTU
+ARG PRIVATE_KEY
+ARG DNS
+ARG PUBLIC_KEY
+ARG ENDPOINT
+ARG ALLOWED_IPS
+
+RUN echo '[Interface] \\n\
+Address = '"$ADDRESS"' \\n\
+MTU = '"$MTU"' \\n\
+PrivateKey = '"$PRIVATE_KEY"' \\n\
+DNS = '"$DNS"' \\n\
+PreUp = sysctl -w net.ipv4.ip_forward=1 \\n\
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE \\n\ 
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth1 -j MASQUERADE \\n\
+\\n\
+[Peer] \\n\
+PublicKey = '"$PUBLIC_KEY"' \\n\
+Endpoint = '"$ENDPOINT"' \\n\
+PersistentKeepalive = 21 \\n\
+AllowedIPs = '"$ALLOWED_IPS" > /config/wg0.conf
+
+RUN cat /config/wg0.conf
+""".encode(
+            "utf-8"
+        )
+    )
