25163783e21d51cca475bbafda06ec4866dd4692
Robert Stein <robert@blueshoe.de>
2023-01-19 10:49:49 +0100

    chore: bump version to 1.0.1
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- client/gefyra/configuration.py
+++ client/gefyra/configuration.py
@@ -11,7 +11,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("gefyra")
 logger.addHandler(console)
 
-__VERSION__ = "1.0.0"
+__VERSION__ = "1.0.1"
 
 
 def fix_pywin32_in_frozen_build() -> None:  # pragma: no cover
