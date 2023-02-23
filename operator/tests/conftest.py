75d0410cacd57cf9b124d4563feef96e897a4c9c
Michael Schilonka <michael@blueshoe.io>
2023-02-23 11:29:39 +0100

    test: add first operator boot test
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

--- operator/tests/conftest.py
+++ operator/tests/conftest.py
@@ -1,21 +1,32 @@
 import logging
+import os
+from time import sleep
 import pytest
-from pytest_kubernetes.providers import AClusterManager
-
-from kopf.testing import KopfRunner
-
+from pytest_kubernetes.providers import AClusterManager, select_provider_manager
+    
 @pytest.fixture
-def operator(k8s: AClusterManager):
+def k3d():
+    k8s: AClusterManager = select_provider_manager("k3d")("gefyra")
     k8s.create()
     k8s.kubectl(["create", "ns", "gefyra"])
+    k8s.wait("ns/gefyra", "jsonpath='{.status.phase}'=Active")
+    os.environ["KUBECONFIG"] = str(k8s.kubeconfig)
+    yield k8s
+    k8s.delete()
+
+@pytest.fixture
+def operator(k3d):
+    from kopf.testing import KopfRunner
     operator = KopfRunner(["run", "-A", "--dev", "main.py"])
     operator.__enter__()
     kopf_logger = logging.getLogger("kopf")
-    kopf_logger.setLevel(logging.CRITICAL)
-    beiboot_logger = logging.getLogger("gefyra")
-    beiboot_logger.setLevel(logging.CRITICAL)
-    yield (operator, k8s)
-    operator.__exit__(None, None, None)
-    k8s.delete()
-    
-    
+    kopf_logger.setLevel(logging.INFO)
+    gefyra_logger = logging.getLogger("gefyra")
+    gefyra_logger.setLevel(logging.INFO)
+
+    yield k3d
+
+    try:
+        operator.__exit__(None, None, None)
+    except:
+        pass
--- operator/tests/e2e/test_run_operator.py
+++
@@ -1,6 +0,0 @@
-from pytest_kubernetes.providers import AClusterManager
-
-def test_a_run_operator(operator):
-    operator, k8s = operator
-    namespaces = k8s.kubectl(["get", "ns"], as_dict=False)
-    print(namespaces)
