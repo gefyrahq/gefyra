6b718e64ec195c179d01389de462c9e5efa077cb
Robert Stein <robert@blueshoe.de>
2023-05-26 15:24:21 +0200

    chore: bump version to 1.1.1

--- client/gefyra/configuration.py
+++ client/gefyra/configuration.py
@@ -12,7 +12,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("gefyra")
 logger.addHandler(console)
 
-__VERSION__ = "1.1.0"
+__VERSION__ = "1.1.1"
 
 
 def fix_pywin32_in_frozen_build() -> None:  # pragma: no cover
