f85ab96927632bbc3f500c7aadcff1d8ccad773c
Robert Stein <robert@blueshoe.de>
2023-06-05 14:56:59 +0200

    chore: draft run command to cli package

--- client/gefyra/cli/__main__.py
+++ client/gefyra/cli/__main__.py
@@ -1,3 +1,4 @@
+from gefyra.__main__ import get_client_configuration
 from .utils import AliasedGroup
 
 import click
@@ -50,6 +51,83 @@ def version(ctx):
     from gefyra.configuration import __VERSION__
 
     print_formatted_text("Gefyra version: " + __VERSION__)
+
+
+@cli.command()
+@click.option(
+    "-d",
+    "--detach",
+    help="Run container in background and print container ID",
+    type=bool,
+    default=False,
+    is_flag=True,
+)
+@click.option(
+    "--rm",
+    "auto_remove",
+    help="Automatically remove the container when it exits",
+    type=bool,
+    is_flag=True,
+    default=False,
+)
+@click.option(
+    "-p",
+    "--expose",
+    help="Add port mapping in form of <container_port>:<host_port>",
+    type=str,
+    multiple=True,
+)  # TODO IpPortMappingParser
+@click.option(
+    "--env_from",
+    help="Copy the environment from the container in the notation 'Pod/Container'",
+    type=str,
+)
+@click.option(
+    "-v",
+    "--volume",
+    help="Bind mount a volume into the container in notation src:dest, allowed multiple times",
+    type=str,
+    multiple=True,
+)
+@click.option(
+    "--env",
+    help="Set or override environment variables in the form ENV=value, allowed multiple times",
+    type=str,
+    multiple=True,
+)
+@click.option(
+    "-n", "--namespace", help="The namespace for this container to run in", type=str
+)
+@click.option(
+    "-c", "--command", help="The command for this container to in Gefyra", type=str
+)
+@click.option(
+    "-N",
+    "--name",
+    help="The name of the container running in Gefyra",
+    type=str,
+    required=True,
+)
+@click.option(
+    "-i", "--image", help="The docker image to run in Gefyra", type=str, required=True
+)
+def run(
+    detach, auto_remove, expose, env_from, volume, env, namespace, command, name, image
+):
+    configuration = ClientConfiguration()
+    api.run(
+        image=image,
+        name=name,
+        command=" ".join(command) if command else None,
+        namespace=namespace,
+        env_from=env_from,
+        env=env,
+        ports=expose,
+        auto_remove=auto_remove,
+        volumes=volume,
+        config=configuration,
+        detach=detach,
+    )
 
 
 def main():
