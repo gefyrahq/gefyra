74e73ded6312752e6f1d1f9a89137c9658402051
Robert Stein <robert@blueshoe.io>
2024-04-19 14:19:43 +0200

    chore: better path handling in get_version.py

--- .github/scripts/get_version.py
+++ .github/scripts/get_version.py
@@ -11,5 +11,6 @@ with open("pyproject.toml") as f:
 
             with open(env_file, "a") as myfile:
                 myfile.write("PYAPP_PROJECT_VERSION=" + version + "\n")
-                myfile.write(f"PYAPP_PROJECT_PATH=./dist/gefyra-{version}.tar.gz\n")
+                path = os.path.abspath(f"./dist/gefyra-{version}.tar.gz")
+                myfile.write(f"PYAPP_PROJECT_PATH={path}\n")
             exit(0)
