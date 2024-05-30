1861a77a5e7d2c121246e48770f055478e108645
Robert Stein <robert@blueshoe.io>
2024-05-30 09:14:10 +0200

    chore: undo connection timeout increase

--- client/gefyra/api/connect.py
+++ client/gefyra/api/connect.py
@@ -123,7 +123,7 @@ def connect(  # noqa: C901
 
     # busy wait for the client to enter the ACTIVE state
     _i = 0
-    while _i < 300:
+    while _i < config.CONNECTION_TIMEOUT:
         if client.state == GefyraClientState.ACTIVE:
             break
         else:
