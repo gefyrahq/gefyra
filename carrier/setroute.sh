23c65ad3a6936b7034926031c8d502f51484df97
Robert Stein <robert@blueshoe.io>
2025-01-16 17:41:27 +0100

    chore: add user / cluster upstream to nginx carrier image

--- carrier/setroute.sh
+++ carrier/setroute.sh
@@ -5,9 +5,13 @@ set -e
 
 DEFAULT_CONF_FILE="/tmp/nginx.conf"
 
+# $1 is the listening port
+# $2 is the user upstream
+# $3 is the cluster upstream
+# If header X-Gefyra-Bridge-Traffic is True, then the request is sent to the user upstream, otherwise it is sent to the cluster upstream.
 
 echo "Setting listening port to $1; Setting target upstream to $2"
-block="upstream stowaway-$1 {server $2;} server {listen $1; proxy_pass stowaway-$1;}\n#MARKER"
+block="map $http_x_gefyra_bridge_traffic $gefyra_backend { \"True\"  $2; default  $3; }server {listen $1; proxy_pass http://$gefyra_backend;}\n#MARKER"
 sed -i "s/#MARKER/$block/g" $DEFAULT_CONF_FILE
 
 nginx -s reload -c $DEFAULT_CONF_FILE
