52bde5512032badf111aae7c3f2c31a89b5f83e4
Robert Stein <robert@blueshoe.io>
2025-07-26 12:19:27 +0200

    chore(#821): remove monkeypatch

--- client/tests/unit/test_operator_install.py
+++ client/tests/unit/test_operator_install.py
@@ -1,12 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-import pytest
-
 from click.testing import CliRunner
 
 
-@pytest.mark.usefixtures("monkeypatch")
 class GefyraOperatorInstallTest(TestCase):
 
     @patch("gefyra.misc.comps.deployment.data")
