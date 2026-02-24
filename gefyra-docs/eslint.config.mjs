ed2694517c44406e57da14fad0338b264dc07189
Lukas Rüsche <lukas@blueshoe.de>
2026-02-24 16:52:38 +0100

    refactor(#GEF-1): update ESLint configuration and package metadata
    - Changed project name in package.json and package-lock.json from "docus-starter" to "gefyra-docs".
    - Added ignores for content in ESLint configuration to streamline linting.
    - Improved formatting in README and other markdown files for consistency.
    - Updated SCSS import syntax for better compatibility.

--- gefyra-docs/eslint.config.mjs
+++ gefyra-docs/eslint.config.mjs
@@ -6,13 +6,13 @@ export default withNuxt(
   antfu({
     vue: true,
     formatters: {
-      markdown: false,
       css: true,
     },
     rules: {
       'no-console': 'warn',
-      'vue/max-attributes-per-line': 'warn',
-      'vue/require-default-prop': 'warn',
     },
+    ignores: [
+      'content/**',
+    ],
   }),
 )
