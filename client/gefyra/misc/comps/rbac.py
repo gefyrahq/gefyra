29ea2b4ce52abce0ec5e0a6af1457d3534534bbd
Michael Schilonka <Schille@users.noreply.github.com>
2026-06-05 16:58:48 +0200

    fix(#GO-1042): adds missing autoscaling apigroup in RBAC (#1118)
    * fix(#GO-1042): adds missing autoscaling apigroup in RBAC
    
    * fix: hpa resource name in cluster role
    
    ---------
    
    Co-authored-by: Robert Stein <robert@blueshoe.io>

--- client/gefyra/misc/comps/rbac.py
+++ client/gefyra/misc/comps/rbac.py
@@ -44,6 +44,7 @@ def data(params: "GefyraInstallOptions") -> list[dict]:
                         "events.k8s.io",
                         "rbac.authorization.k8s.io",
                         "rbac.authorization.k8s.io/v1",
+                        "autoscaling",
                     ],
                     "resources": [
                         "namespaces",
@@ -61,7 +62,7 @@ def data(params: "GefyraInstallOptions") -> list[dict]:
                         "pods/exec",
                         "pods/status",
                         "events",
-                        "hpas",
+                        "horizontalpodautoscalers",
                     ],
                     "verbs": ["*"],
                 },
